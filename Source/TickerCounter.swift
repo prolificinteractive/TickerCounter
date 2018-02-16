df584e7f57fd2e94c266380a9dd12b603b2a305c
Harlan Kellaway <hello@harlankellaway.com>
2018-02-16 15:58:52 -0500

    Added test file

---
+++ Source/TickerCounter.swift
@@ -0,0 +1,24 @@
+//
+//  TickerCounter.swift
+//  TickerCounterTests
+//
+//  Created by Claire Lynch on 2/16/18.
+//  Copyright © 2018 Prolific Interactive. All rights reserved.
+//
+
+import UIKit
+
+/// Ticker counter view.
+public class TickerCounter: UIView {
+    
+    // MARK: - Init/Deinit
+    
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+}
